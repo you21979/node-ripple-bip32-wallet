8440a3da3a9ea3a26aac39fe96b95cd6fa202504
you21979 <you2197901@gmail.com>
2016-07-15 18:46:32 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var bip39 = require("bip39");
+var keypairs = require('ripple-keypairs');
+
+var generateAddress = function(options) {
+  var secret = keypairs.generateSeed(options);
+  var keypair = keypairs.deriveKeypair(secret);
+  var address = keypairs.deriveAddress(keypair.publicKey);
+  return { secret: secret, address: address };
+}
+
+var mnemonicToRippleAddress = module.exports = function(mnemonic){
+    var masterseed = bip39.mnemonicToSeed(mnemonic);
+    return rippleAddress = generateAddress({ entropy: masterseed })
+}
+
